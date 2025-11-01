17b92296a3a4143eefc40a65fe1906077dd11957
demouser2002 <demo2002email@gmail.com>
2025-11-01 22:20:00 +0530

    Update log message for server port

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 8080
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
